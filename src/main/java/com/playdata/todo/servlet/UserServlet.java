c86849deb462470f1c507818ce88198f1cce9933
gotoAbraxas <lty7085@gmail.com>
2023-06-15 10:48:17 +0900

    userPost

--- src/main/java/com/playdata/todo/servlet/UserServlet.java
+++ src/main/java/com/playdata/todo/servlet/UserServlet.java
@@ -10,12 +10,21 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(200);
         req.getRequestDispatcher("views/user.html").forward(req,resp);
-        super.doGet(req, resp);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+       String username =  req.getParameter("username");
+       String password = req.getParameter("password");
+       String name = req.getParameter("name");
+       resp.setStatus(201);
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(name);
+        resp.sendRedirect("/user");
+   //    super.doPost(req, resp);
     }
 }
